b0bb8c0844affdf1fd96e099eb0c80392854f47b
b3nzuk3 <hussenito7@gmail.com>
2022-05-11 01:13:04 +0300

    created a logout route to logout user

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -65,3 +65,7 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route("/account")
+def acoount():
+    return render_template('account.html', title='Account')
