fb05c6a0ed3ca07366b52f820038a3aed6bff09e
b3nzuk3 <hussenito7@gmail.com>
2022-05-11 00:22:36 +0300

    user is able to login true User authentication

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
-
-app= Flask(__name__)
-
-app.config['SECRET_KEY'] = '5c6b8cd67af797b778498e02f5619ef3'
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
+login_manager = LoginManager(app)
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,53 +2,66 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user
 
-posts =[
+
+posts = [
     {
-        'heading':'dungeon',
-        'author':'hussein',
-        'address': 'oxford street',
-        'content': 'This is not going to okay but dreadful'
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
     },
     {
-        'heading':'Death',
-        'author':'jamal',
-        'address':'newyork street',
-        'content': 'This is going to be a fun one'
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
     }
 ]
 
-@app.route('/')
-@app.route('/home')
+
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html', title ='about')
+    return render_template('about.html', title='About')
 
 
-@app.route('/register', methods = ['GET', 'POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password = hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f'Account created for {form.username.data}, You are now able to Login!', 'success')
+        flash(f'Account created for {form.username.data}!, You can now login.','success')
         return redirect(url_for('login'))
-    return render_template('register.html', title = 'Register', form = form)
+    return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember= form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login unsuccessful. Please check username and password!', 'danger')
+            flash('Login Unsuccessful. Please check Email and Password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
-    return render_template('login.html', title = 'Login', form = form)
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
